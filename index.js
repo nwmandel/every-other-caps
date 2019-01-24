9072e5cce04e3c7a8d6abe201c37d6301b6ec727
Nicholas Mandel <nwmandel@gmail.com>
2019-01-23 23:03:48 -0800

    more tests and more cases

--- index.js
+++ index.js
@@ -3,26 +3,54 @@ module.exports = inputs => {
   if (typeof inputs === 'string') {
     const lowercase = inputs.toLocaleLowerCase();
     let result = '';
+
     // case for evens
     if (lowercase.length % 2 == 0) {
       result = cap(lowercase);
+
     // case for odds
     } else {
       result = cap(lowercase);
     }
+
     return result;
   }
+  
+  throw new TypeError('Input must be string');
 };
 
-// function to capitalize every other character
-const cap = words => {
+// function to capitalize every other letter
+const cap = word => {
+
+  // cap every other alphabetic character
+
   let result = '';
-  for (let i = 0; i < words.length; i++) {
-    if (i % 2 !== 0 && i > 0) {
-      result += words.charAt(i).toUpperCase();
+  let nextUp = word.charAt(0) === word.charAt(0).toLowerCase();   
+
+  for (let i = 0; i < word.length; i++) {
+    const currLetter = word.charAt(i);
+    
+    // case when previous letter was lowercase or first letter
+    if (/[a-z]/.test(currLetter) && i !== 0 && nextUp) {
+      result += currLetter.toUpperCase();
+      nextUp = false;
+
+    // case when previous letter is not lowercase
+    } else if (/[a-z]/.test(currLetter)) {
+      result += currLetter;
+      nextUp = true;
     } else {
-      result += words.charAt(i);
+      result += currLetter;
+
+      // if current letter is whitespace don't cap next letter
+      if (/\s/.test(currLetter)) {
+        nextUp = false;
+      } else if (!/\s/.test(currLetter) && !/[a-z]/.test(currLetter)) {
+        nextUp = true;
+      }
+      
     }
+       
   }
   return result;
 };
--- test.js
+++ test.js
@@ -8,6 +8,36 @@ describe('tests for module', () => {
       expect(eoc('aa')).to.be.a('string');
     });
   });
+
+  describe('number', () => {
+    it('should not work for type number', () =>{
+      expect(() => eoc(1).to.throw(TypeError)); 
+    });
+  });
+
+  describe('array', () => {
+    it('should not work for type array', () =>{
+      expect(() => eoc(['a','b','c']).to.throw(TypeError)); 
+    });
+  });
+
+  describe('object', () => {
+    it('should not work for type object', () =>{
+      expect(() => eoc({ 'a': 1 }).to.throw(TypeError)); 
+    });
+  });
+
+  describe('null', () => {
+    it('should not work for type null', () =>{
+      expect(() => eoc(null).to.throw(TypeError)); 
+    });
+  });
+
+  describe('undefined', () => {
+    it('should not work for type undefined', () =>{
+      expect(() => eoc(undefined).to.throw(TypeError)); 
+    });
+  });
   
   describe('string length', () => {
     it('should be even', () => {
@@ -16,20 +46,32 @@ describe('tests for module', () => {
   });
 
   describe('string length', () => {
-    it('should be even', () => {
+    it('should be odd', () => {
       expect(eoc('aaa').length).to.equal(3);
     });
   });
 
   describe('string should start with lowercase then every other character should be capital', () => {
-    it('should be odd', () => {
+    it('should be correct', () => {
       expect(eoc('aaaa')).to.equal('aAaA');
     });
   });
 
   describe('string should start with lowercase then every other character should be capital', () => {
-    it('should be odd', () => {
+    it('should be correct', () => {
       expect(eoc('aaaaa')).to.equal('aAaAa');
+    });
+  });
+
+  describe('string should start with lowercase then every other character should be capital', () => {
+    it('should be correct', () => {
+      expect(eoc('aaa  bbb c aaa')).to.equal('aAa  bBb c aAa');
+    });
+  });
+
+  describe('string should start with lowercase then every other character should be capital', () => {
+    it('should be correct', () => {
+      expect(eoc('aaa@ #i  bbb []1c aaa')).to.equal('aAa@ #I  bBb []1C aAa');
     });
   });
 
