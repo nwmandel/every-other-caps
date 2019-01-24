d3a1adacb09fa5c195af7cfccc94de6977eceed8
Nicholas Mandel <nwmandel@gmail.com>
2019-01-23 21:01:38 -0800

    adding semicolons

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
-'use-strict'
+'use-strict';
 module.exports = inputs => {
   if (typeof inputs === 'string') {
-    const lowercase = inputs.toLocaleLowerCase()
-    let result = ''
+    const lowercase = inputs.toLocaleLowerCase();
+    let result = '';
     // case for evens
     if (lowercase.length % 2 == 0) {
-      result = cap(lowercase)
+      result = cap(lowercase);
     // case for odds
     } else {
-      result = cap(lowercase)
+      result = cap(lowercase);
     }
-    return result
+    return result;
   }
-}
+};
 
 // function to capitalize every other character
 const cap = words => {
-  let result = ''
+  let result = '';
   for (let i = 0; i < words.length; i++) {
     if (i % 2 !== 0 && i > 0) {
-      result += words.charAt(i).toUpperCase()
+      result += words.charAt(i).toUpperCase();
     } else {
-      result += words.charAt(i)
+      result += words.charAt(i);
     }
   }
-  return result
-}
+  return result;
+};
--- test.js
+++ test.js
@@ -1,37 +1,36 @@
-const assert = require('assert')
-const expect = require('chai').expect
-const eoc = require('.')
+const assert = require('assert');
+const expect = require('chai').expect;
+const eoc = require('.');
 
 describe('tests for module', () => {
   describe('string', () => {
     it('should be type string', () =>{
-      expect(eoc('aa')).to.be.a('string')
-    })
-  })
+      expect(eoc('aa')).to.be.a('string');
+    });
+  });
   
   describe('string length', () => {
     it('should be even', () => {
-      expect(eoc('aaaa').length).to.equal(4)
+      expect(eoc('aaaa').length).to.equal(4);
     })
-  })
+  });
 
   describe('string length', () => {
     it('should be even', () => {
-      expect(eoc('aaa').length).to.equal(3)
-    })
-  })
+      expect(eoc('aaa').length).to.equal(3);
+    });
+  });
 
   describe('string should start with lowercase then every other character should be capital', () => {
     it('should be odd', () => {
-      expect(eoc('aaaa')).to.equal('aAaA')
-    })
-  })
+      expect(eoc('aaaa')).to.equal('aAaA');
+    });
+  });
 
   describe('string should start with lowercase then every other character should be capital', () => {
     it('should be odd', () => {
-      expect(eoc('aaaaa')).to.equal('aAaAa')
-    })
-  })
+      expect(eoc('aaaaa')).to.equal('aAaAa');
+    });
+  });
 
-})
-
+});
